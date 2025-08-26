333e15b02ca17adb3ccbc555076f06229f0674b2
Lucas Araujo <assuncao.araujo.lucas@gmail.com>
2025-08-26 20:20:29 -0300

    feat: add Home page and shared components
    - Introduced HomeComponent with associated HTML, SCSS, and spec files.
    - Created shared BannerComponent, CardBuscaComponent, CardComponent, and ContainerComponent with their respective templates, styles, and tests.
    - Updated app.module.ts to include new components and Angular Material modules.
    - Enhanced index.html and global styles for typography and layout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BannerComponent } from './shared/banner/banner.component';
+import { CardBuscaComponent } from './shared/card-busca/card-busca.component';
+import { CardComponent } from './shared/card/card.component';
+import { ContainerComponent } from './shared/container/container.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    CardComponent,
+    ContainerComponent,
+    HomeComponent,
+    CardBuscaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]

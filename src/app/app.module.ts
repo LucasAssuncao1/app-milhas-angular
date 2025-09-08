5e1fec69e9c610205158bcbfbaf048b38c1a1cd9
Lucas Araujo <assuncao.araujo.lucas@gmail.com>
2025-09-08 19:06:09 -0300

    feat: adicionar componente de login com template, estilos e testes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ import { HeaderComponent } from './shared/header/header.component';
 import { ModalComponent } from './shared/modal/modal.component';
 import { DepoimentosComponent } from './pages/home/depoimentos/depoimentos.component';
 import { SeletorPassageiroComponent } from './shared/seletor-passageiro/seletor-passageiro.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
@@ -51,7 +52,8 @@ import { SeletorPassageiroComponent } from './shared/seletor-passageiro/seletor-
     PromocoesComponent,
     DropdownUfComponent,
     DepoimentosComponent,
-    SeletorPassageiroComponent
+    SeletorPassageiroComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
